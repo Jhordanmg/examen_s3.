a41673bb8b7f003a101be86386fc39054e01e31e
Jhordanmg <77113331+Jhordanmg@users.noreply.github.com>
2021-01-14 11:59:28 -0500

    Add files via upload

---
+++ EV3_Mendoza Gómez, Jhordan Samiel/js/pregunta3.js
@@ -0,0 +1,39 @@
+function uno(){
+    console.log("Mesa1");
+}
+
+function dos(){
+    console.log("Mesa2");
+}
+
+function tres(){
+    console.log("Mesa3");
+}
+
+function cuatro(){
+    console.log("Mesa4");
+}
+
+function cinco(){
+    console.log("Mesa 5");
+}
+
+function seis(){
+    console.log("Mesa6");
+}
+
+function siete(){
+    console.log("Mesa7");
+}
+
+function ocho(){
+    console.log("Mesa8");
+}
+
+function nueve(){
+    console.log("Mesa9");
+}
+
+function diez(){
+    console.log("Mesa10");
+}
